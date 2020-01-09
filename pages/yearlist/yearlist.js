194d02262cd4eb24eeac00a9188721c2051b5a7d
unknown <lijiahui@etiantian.com>
2020-01-09 14:22:29 +0800

    页面滑动效果

--- pages/yearlist/yearlist.js
+++ pages/yearlist/yearlist.js
@@ -3,9 +3,17 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
+    animationData: {},
     userId: null,
     userText: '',
-    currentTime: ''
+    currentTime: '',
+    scrollindex:0,  //当前页面的索引值
+    totalnum:5,  //总共页面数
+    starty:0,  //开始的位置x
+    endy:0, //结束的位置y
+    critical: 100, //触发翻页的临界值
+    margintop:0,  //滑动下拉距离
+    hasPage4: true
   },
   onLoad: function (options) {
     console.log(options);
@@ -19,6 +27,62 @@ Page({
     var value = util.formatTime(new Date())
     this.setData({
       currentTime: value
+    })
+  },
+  scrollTouchstart: function(e) {
+    let py = e.touches[0].pageY;
+    this.setData({
+      starty: py,
+      endy: py
+    })
+  },
+  scrollTouchmove: function(e) {
+    let py = e.touches[0].pageY;
+    let d = this.data;
+    this.setData({
+      endy: py,
+    })
+    if (py-d.starty<100 && py-d.starty>-100) {    
+      this.setData({
+        margintop: py - d.starty
+      })
+    }
+  },
+  scrollTouchend: function(e) {
+    let d = this.data;
+    if (d.endy-d.starty >100 && d.scrollindex>0) {
+      this.setData({
+        scrollindex: d.scrollindex-1
+      })
+    } else if (d.endy-d.starty <-100 && d.scrollindex<this.data.totalnum-1) {
+      this.setData({
+        scrollindex: d.scrollindex+1
+      })
+    }
+    this.setData({
+      starty:0,
+      endy:0,
+      margintop:0
+    })
+  },
+  saveImg: function() {
+    console.log(123111)
+    wx.downloadFile({
+      url: 'https://t7.baidu.com/it/u=1179872664,290201490&fm=79&app=86&f=JPEG?w=1280&h=854',
+      success: function(res) {
+        console.log(res)
+        wx.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath,
+          success: function() {
+            wx.showToast({
+              title: '保存成功',
+            })
+          }
+        })
+      },
+      fail: function(e) {
+        console.log(e)
+      }
     })
   },
   onShareAppMessage: function() {
